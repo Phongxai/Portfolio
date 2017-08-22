2194a5906ffb2029ccf6454dd2ba29209d619f22
Phongxai Vang <phongxai.vang@gmail.com>
2017-08-22 12:54:11 -0500

    Update

--- redesign/js/script.js
+++ redesign/js/script.js
@@ -1,9 +1,47 @@
-$(window).scroll(function(){
+$(function() {
+
+  var dHeight = $('.card .card-image img').height();
+
+  $(".card-hover").css({
+    'height': dHeight + 'px'
+  });
+
+  setBindings();
+
+});
+
+$(window).resize(function() {
+
+  var dHeight = $('.card .card-image img').height();
+
+  $(".card-hover").css({
+    'height': dHeight + 'px'
+  });
+
+});
+
+$(window).scroll(function() {
 
   var wScroll = $(this).scrollTop();
 
   $('.cd-intro').css({
-    'transform' : 'translate(0px, ' + wScroll / -100 +'%)'
+    'transform': 'translate(0px, ' + wScroll / -100 + '%)'
+  });
+
+  $('.hero-image').css({
+    'transform': 'translate(0px, ' + wScroll / -80 + '%)'
   });
 
 });
+
+function setBindings() {
+  $(".main-nav ul li a, .nav-button").click(function(e) {
+    e.preventDefault();
+
+    var sectionID = e.currentTarget.id + "Section";
+
+    $("html body").animate({
+      scrollTop: $("#" + sectionID).offset().top
+    }, 1000);
+  });
+}
