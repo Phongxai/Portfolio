dcd6349f7bd436de4baf6dd5d11cb18110a29af9
Phongxai Vang <phongxai.vang@gmail.com>
2017-10-29 14:31:36 -0500

    test

--- redesign/js/script.js
+++ redesign/js/script.js
@@ -1,17 +1,42 @@
-jQuery(window).load(function() {
+$(document).ready(function () {
+    setBindings();
+    // openNav();
+    linkNav();
+});
+
+function setBindings() {
+
+    try {
+        $(".overlay-content a").click(function (e) {
+            e.preventDefault();
+            //   alert("clicked");
     
-});
+            var sectionID = e.currentTarget.id + "Section";
+            var linkHref = $(this).attr('href');
+            //   console.log(sectionID);
+            // console.log($(linkHref).offset().top);
+    
+            $("html, body").animate({
+                scrollTop: $(linkHref).offset().top
+            });
+        });
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
 
-
-$(document).ready(function() {
-    $('.openNav').click(function () {
-        openNav();        
-    });
-});
 
 function openNav() {
-    $('.openNav').css({
-        "width" : "100%"
+    document.getElementById('nav').style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById('nav').style.width = "0%";
+}
+
+function linkNav() {
+    $(".overlay-content a").click(function () {
+        closeNav();
     });
-    console.log("hello");
 }
